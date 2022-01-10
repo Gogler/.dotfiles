ee4d385f0243196504b5b5465abc5136db6f51f3
Adam Perdeusz <a.gogler@gmail.com>
2022-01-10 20:23:05 +0100

    Update setup script

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+#alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+#echo ".dotfiles" >> .gitignore
+#git clone --bare <git-repo-url> $HOME/.dotfiles
+#config checkout
+#config config --local status.showUntrackedFiles no
+
+# Terminal colors
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+# Oh my ZSH configuration
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
 mkdir -p "$HOME/.zsh"
+
+# Oh my ZSH plugins
 git clone https://github.com/reobin/typewritten.git "$HOME/.zsh/typewritten"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
