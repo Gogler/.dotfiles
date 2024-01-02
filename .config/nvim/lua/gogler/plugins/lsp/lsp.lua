a954338d975bfa678b8fcd36459ce01e1ea91868
Adam Perdeusz <a.gogler@gmail.com>
2024-01-02 21:19:32 +0100

    Workin neovim setup

--- .config/nvim/lua/gogler/plugins/lsp/lsp.lua
+++ .config/nvim/lua/gogler/plugins/lsp/lsp.lua
@@ -1,47 +1,60 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "clangd",
-                    "cmake",
-                    "jedi_language_server",
-                    "bashls",
-                    "typos_lsp",
-                },
-                automatic_installation = true,
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"cmake",
+					"jedi_language_server",
+					"bashls",
+					"typos_lsp",
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd .setup({})
-            lspconfig.cmake .setup({})
-            lspconfig.jedi_language_server.setup({})
-            lspconfig.bashls .setup({})
-            lspconfig.typos_lsp.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jedi_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.typos_lsp.setup({
+				capabilities = capabilities,
+			})
 
-
-            local opts = {}
-            vim.keymap.set('n', "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        end
-    }
+			local opts = {}
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+		end,
+	},
 }
